404c22fbfe3c6c74bfcf0670a0d31db77779b051
haroonsyed <haroonaminsyed@gmail.com>
2023-06-02 00:18:24 -0400

    Forgot check for over 3 letters

--- index.ts
+++ index.ts
@@ -15,7 +15,7 @@ Object.keys(words).forEach((word)=>{
         contains_required ||= letter == required;
         can_be_made_of_all_letters &&= all_letters.includes(letter);
     })
-    if(contains_required && can_be_made_of_all_letters) {
+    if(contains_required && can_be_made_of_all_letters && word.length > 3) {
         console.log(word);
     }
 })
