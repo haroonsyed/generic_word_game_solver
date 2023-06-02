1f4f64dbbd39b06831e75f71524eb10569eaca25
haroonsyed <haroonaminsyed@gmail.com>
2023-06-02 00:10:58 -0400

    Init (and probably final) commit

---
+++ index.ts
@@ -0,0 +1,23 @@
+import fs from 'fs'
+const words = JSON.parse(fs.readFileSync('./words_dictionary.json', 'utf-8'))
+
+let required = 'y';
+let others = ['e', 'z', 'r', 'l', 'b', 'i'];
+
+console.clear()
+console.log("===========Seeking Words===========")
+
+let all_letters = [...others, required];
+Object.keys(words).forEach((word)=>{
+    let contains_required = false;
+    let can_be_made_of_all_letters = true;
+    [...word].forEach(letter=>{
+        contains_required ||= letter == required;
+        can_be_made_of_all_letters &&= all_letters.includes(letter);
+    })
+    if(contains_required && can_be_made_of_all_letters) {
+        console.log(word);
+    }
+})
+
+console.log("=============Done============")
---
+++ readme.md
@@ -0,0 +1,3 @@
+5 minute script to ruin family game night
+
+Credits to: https://github.com/dwyl/english-words
